992e1a177d38a2e1a1c2738a515a0e64bf3bb6fd
FranciscoEmmanuel1998 <emmanuel.fco0401@gmail.com>
2025-07-28 20:49:54 -0600

    Add epic text overlay animation

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,13 @@
-import UnifiedSimulation from "@/components/simulations/UnifiedSimulation";
+import CombinedSimulation from "@/components/simulations/CombinedSimulation";
+
 const Index = () => {
   return (
     <div className="cosmic-canvas">
       {/* Museo Matemático-Simbólico Inmersivo */}
       <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 p-6 h-screen">
-        
-
         {/* 4. Simulación unificada */}
         <div className="w-screen h-screen">
-          <UnifiedSimulation />
+          <CombinedSimulation />
         </div>
       </div>
     </div>
